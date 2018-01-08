d67057f8b4b056d94f6f45bd260fe45884696a45
Dan Brett <boristhellama99@gmail.com>
2018-01-08 20:55:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,5 +5,6 @@ function addElementToBeginningOfArray (array, atStart) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, asStart) {
-  
+   array.unshift(element);
+   return array;
 }
